33aa93930bafb1b6562a1a68c8b4ff4d933897eb
Andreas Stenius <andreas.stenius@svenskaspel.se>
2014-10-03 20:28:43 +0200

    intial structure taking shape..

---
+++ lib/kelm-cli/src/kelm.erl
@@ -0,0 +1,18 @@
+-module(kelm).
+
+%% kelm: kelm's entry point.
+
+-export([main/1]).
+
+
+%% API
+
+main(Args) ->
+    ok().
+
+%% Internals
+
+ok() ->
+    io:format("Hello from kelm~n").
+
+%% End of Module.
